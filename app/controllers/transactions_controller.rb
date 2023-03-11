b996fe1e383c60f514b7893d4f1c83bd14aff67e
Przemysław Tasarz <przemek433@interia.eu>
2023-03-11 12:03:23 +0100

    request fix

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,6 +1,7 @@
 class TransactionsController < ApplicationController
   before_action :set_transaction, only: %i[ show edit update destroy ]
   before_action :set_default_request_format
+  skip_before_action :verify_authenticity_token
 
   # GET /transactions or /transactions.json
   def index
